bd2823a0f50a04bd9e2ee68987f93cb269f0bba0
Lightening Gibson <lighteningibson@gmail.com>
2022-08-30 17:52:33 -0500

    commit tree 526ea17a5c5e6c0db7408d81f0acb27cfb825ef4

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+
+ const cats = ["Milo", "Otis", "Garfield"]; 
+
+
+
+ function destructivelyAppendCat (name) {
+
+    cats.push(name);
+
+
+ }
+
+ function destructivelyPrependCat(name) {
+
+    cats.unshift(name);
+ }
+ 
+function destructivelyRemoveLastCat(){
+
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+
+    cats.shift();
+}
+function appendCat(name) {
+
+  const appendCatsArray =  [ ...cats, name ];
+
+  return appendCatsArray;
+}
+
+function prependCat(name) {
+
+ const appendCatsArray = [name, ...cats]; 
+
+ return appendCatsArray;
+
+}
+function removeLastCat() {
+
+    const removeLastCatArray = [...cats.slice(0, -1)]
+
+    return removeLastCatArray; 
+}
+
+function removeFirstCat() {
+
+    const removeFirstCatArray = [...cats.slice(1)]
+
+    return removeFirstCatArray;
+}
